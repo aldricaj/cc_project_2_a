a84d61794c6f77a625ed795eb08288a22792bf5d
Andrew Aldrich <aldricaj@mail.uc.edu>
2019-11-20 14:41:42 -0500

    Update Project2B.java

--- Project2B.java
+++ Project2B.java
@@ -35,52 +35,12 @@ public class Project2B {
                 String nodeId = nodeIdStr[1];
                 String graphType = nodeIdStr[0];
                 boolean directed = graphType.equals("dir");
-                //String graphType = directed ? "directed" : "undirected";
+                String graphTypeText = directed ? "directed" : "undirected";
                 String[] adjNodes = inputList[1].split(",");
                 
                 int numAdjNodes = adjNodes.length;
 
-                if (directed) {
-                    Integer directedMaxVal = directedMax.value;
-                    if(directedMax == null || numAdjNodes > directedMaxVal.intValue()) {
-                        directedMax = new Pair(nodeId, numAdjNodes);
-                        directedLongestAdjList = new Pair(nodeId, inputList[1]);
-                    }
-                    Integer directedMinVal = directedMin.value;
-                    if(directedMin == null || numAdjNodes < directedMinVal.intValue()) {
-                        directedMin = new Pair(nodeId, numAdjNodes);
-                    }
-                }
-                else {
-                    Integer undirectedMaxVal = undirectedMax.value;
-                    if(undirectedMax == null || numAdjNodes > undirectedMaxVal.intValue()) {
-                        undirectedMax = new Pair(nodeId, numAdjNodes);
-                        undirectedLongestAdjList = new Pair(nodeId, inputList[1]);
-                    }
-                    Integer undirectedMinVal = undirectedMin.value;
-                    if(undirectedMin == null || numAdjNodes < undirectedMinVal.intValue()) {
-                        undirectedMin = new Pair(nodeId, numAdjNodes);
-                    }
-                }
-
-                //context.write(toText(key), toText(nodeId + "||" + inputList[1] + '||' + numAdjNodes));
-            }
-        }
-
-        public void cleanup(Context c) throws IOException, InterruptedException {
-            if (c != null) {
-                if (undirectedMin != null)
-                    c.write(new Text("undirected_min_connections"), toText(undirectedMin.toString()));
-                if (undirectedMax != null)
-                    c.write(new Text("undirected_max_connections"), toText(undirectedMax.toString()));
-                if (directedMin != null)
-                    c.write(new Text("directed_min_connections"), toText(directedMin.toString()));
-                if (directedMax != null)
-                    c.write(new Text("directed_max_connections"), toText(directedMax.toString()));
-                if (directedLongestAdjList != null)
-                    c.write(new Text("undirected_longest_adj"), toText(directedLongestAdjList.toString()));
-                if (undirectedLongestAdjList != null)
-                    c.write(new Text("directed_longest_adj"), toText(undirectedLongestAdjList.toString()));
+                context.write(toText(directed), toText(nodeId + "||" + inputList[1] + '||' + numAdjNodes));
             }
         }
 
@@ -117,29 +77,8 @@ public class Project2B {
             int resultValue = -1;
             String k = key.toString();
             for (Text v : values) {
-                String[] pair = v.toString().split("||");
+                Row r = new Row(v);
 
-                if (k.contains("min_conn")) {
-                    int val = Integer.parseInt(pair[1]);
-                    if (resultValue == -1 || val < resultValue) {
-                        result = "" + val;
-                        resultValue = val;
-                    }
-                }
-                else if (k.contains("max_conn")) {
-                    int val = Integer.parseInt(pair[1]);
-                    if (resultValue == -1 || val > resultValue) {
-                        result = "" + val;
-                        resultValue = val;
-                    }
-                }
-                else if (k.contains("longest")){
-                    int val = (pair[1].split(",").length);
-                    if (resultValue == -1 || val > resultValue) {
-                        resultValue = val;
-                        result = v.toString();
-                    }
-                }
             }
             context.write(key, toText(result));
         }
@@ -147,6 +86,17 @@ public class Project2B {
             Text t = new Text();
             t.set(s);
             return t;
+        }
+        private class Row {
+            public String adjList;
+            public String id;
+            public int length;
+            public Row(String s) {
+                String[] vars = s.split('||')
+                id = vars[0];
+                adjList = vars[1];
+                length = Integer.parseInt(vars[2]);
+            }
         }
     }
 
